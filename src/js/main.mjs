146e3b29b93269863e1780ad1d8a618d762043da
LeoGC7 <lcorrea@byupathway.edu>
2026-02-11 11:29:47 -0700

    paths changed

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -2,8 +2,8 @@ import { loadHeaderandFooter } from "./utils.mjs";
 import { displaySelectOptions, displayFavoriteExchanges, initFavoriteClick, getExchangeRate } from "./currency.mjs";
 import { displayCountries } from "./news.mjs";
 
-const currencies = '/json/currency.json';
-const countries = '/json/country.json'
+const currencies = './json/currency.json';
+const countries = './json/country.json'
 const inputSelectElement = document.getElementById('currencyInput');
 const targetSelectElement = document.getElementById('currencyTarget');
 const countrySelectElement = document.getElementById('countrySelect');
